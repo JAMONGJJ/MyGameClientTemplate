d129e8e510dcc26f759f8e3eb725e54adb9e7159
JAMONGJJ <jamongjj@kakao.com>
2022-10-17 21:59:27 +0900

    Add EntitySystem

---
+++ Assets/02.Scripts/EntitySystem/EntitySystem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ClientTemplate
+{
+    public static class Entity
+    {
+        public static EntitySystem System { get; } = new EntitySystem();
+    }
+    public class EntitySystem
+    {
+
+    }
+}
