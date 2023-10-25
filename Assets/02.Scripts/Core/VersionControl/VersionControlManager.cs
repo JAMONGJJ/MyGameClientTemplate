dead09d8534d051749890eddcc3b40503733f2c8
JAMONGJJ <jamongjj@kakao.com>
2023-10-25 18:57:36 +0900

    version type 코드 수정

--- Assets/02.Scripts/Core/VersionControl/VersionControlManager.cs
+++ Assets/02.Scripts/Core/VersionControl/VersionControlManager.cs
@@ -39,7 +39,7 @@ namespace ClientTemplate
 
         public VersionType GetVersionType()
         {
-            VersionType result = VersionType.Update;
+            VersionType result = VersionType.Play;
             string version = Application.version;
             // TODO : version 문자열에 따라서 Play, Update, Block 구분하는 코드 작성 필요
             return result;
