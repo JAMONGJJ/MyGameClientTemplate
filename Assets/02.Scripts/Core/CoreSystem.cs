3a176edf7f2aea6a8e3269c5bd2f7d996384d877
JAMONGJJ <jamongjj@kakao.com>
2023-10-31 00:59:07 +0900

    코드 정리
    - 주석 수정
    - 안 쓰는 스크립트 삭제

--- Assets/02.Scripts/Core/CoreSystem.cs
+++ Assets/02.Scripts/Core/CoreSystem.cs
@@ -9,9 +9,7 @@ namespace ClientTemplate
         public static CoreSystem System { get; } = new CoreSystem();
     }
     
-    /// <summary>
-    /// KlayLand가 아니어도 있을 법한 공통적인 기능들을 모아놓은 클래스
-    /// </summary>
+
     public class CoreSystem
     {
         public IDataManufactureManager DataManufacture { get; private set; }    // 데이터 가공 기능을 수행(서버에서 받아온 데이터, 서버로 보낼 데이터, Model <-> View 전환 등)
@@ -20,7 +18,7 @@ namespace ClientTemplate
         public ISettingsManager Settings { get; private set; }  // 게임 설정값(프레임레이트, 언어 등) 관리
         public INetworkManager Network { get; private set; }    // 서버 통신 관리
         public IAuthenticationManager Authentication { get; private set; }    // 사용자 인증 관리
-        public IVersionControlManager Version { get; private set; }     // 
+        public IVersionControlManager Version { get; private set; }     // 앱 버전과 그와 관련된 기능을 관리
 
         public void SetResourceManager(IResourceManager manager)
         {
